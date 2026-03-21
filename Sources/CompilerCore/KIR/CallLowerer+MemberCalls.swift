910e8171a5eada107d131234003414acafa3be24
fumiya-kume <fumiya.kume@hotmail.com>
2026-03-21 18:00:19 +0900

    Fix diff regressions for bit counts, byte arrays, and map by-or-null

--- Sources/CompilerCore/KIR/CallLowerer+MemberCalls.swift
+++ Sources/CompilerCore/KIR/CallLowerer+MemberCalls.swift
@@ -1726,10 +1726,12 @@ extension CallLowerer {
                     ("kk_string_dropLast", [loweredReceiverID, loweredArgIDs[0]])
                 case "chunked":
                     ("kk_string_chunked", [loweredReceiverID, loweredArgIDs[0]])
-                case "encodeToByteArray":
-                    ("kk_string_encodeToByteArray_charset", [loweredReceiverID, loweredArgIDs[0]])
-                case "toByteArray":
-                    ("kk_string_toByteArray_charset", [loweredReceiverID, loweredArgIDs[0]])
+                case "encodeToByteArray", "toByteArray":
+                    if loweredArgIDs.count == 1 {
+                        ("kk_string_encodeToByteArray_charset", [loweredReceiverID, loweredArgIDs[0]])
+                    } else {
+                        ("kk_string_encodeToByteArray_range", [loweredReceiverID, loweredArgIDs[0], loweredArgIDs[1]])
+                    }
                 case "commonPrefixWith":
                     if loweredArgIDs.count >= 2 {
                         ("kk_string_commonPrefixWith_ignoreCase", [loweredReceiverID, loweredArgIDs[0], loweredArgIDs[1]])
