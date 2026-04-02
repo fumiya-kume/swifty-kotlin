cfbee68e84dee74e2d92491568ca397253321a73
fumiya-kume <fumiya.kume@hotmail.com>
2026-04-02 10:58:12 +0900

    Implement kotlin.synchronized end-to-end

--- Tests/RuntimeTests/ABIMismatchTests+SyntheticStubParity.swift
+++ Tests/RuntimeTests/ABIMismatchTests+SyntheticStubParity.swift
@@ -39,6 +39,7 @@ extension ABIMismatchTests {
             "kk_system_process_start_nanos",
             "kk_system_measureTimeMillis",
             "kk_system_measureNanoTime",
+            "kk_synchronized",
             // Atomic (kotlin.concurrent)
             "kk_atomic_int_create",
             "kk_atomic_int_load",
