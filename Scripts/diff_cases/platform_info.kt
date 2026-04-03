cacf549fc85b7b5282489152eb887b41edb108a3
fumiya-kume <fumiya.kume@hotmail.com>
2026-04-03 12:39:32 +0900

    Skip native platform diff case

--- Scripts/diff_cases/platform_info.kt
+++ Scripts/diff_cases/platform_info.kt
@@ -1,3 +1,4 @@
+// SKIP-DIFF: kotlin.native.* APIs are not available in the kotlinc diff reference environment.
 import kotlin.native.CpuArchitecture
 import kotlin.native.OsFamily
 import kotlin.native.Platform
