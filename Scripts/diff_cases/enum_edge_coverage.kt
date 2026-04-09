e350c9dfc613770534e669c77037b1ede8dd354d
fumiya-kume <fumiya.kume@hotmail.com>
2026-04-10 02:08:32 +0900

    Add enum edge case coverage

---
+++ Scripts/diff_cases/enum_edge_coverage.kt
@@ -0,0 +1,18 @@
+enum class Direction {
+    NORTH,
+    SOUTH,
+}
+
+fun main() {
+    println(Direction.entries.toList())
+    println(enumValues<Direction>().toList())
+    println(enumValueOf<Direction>("NORTH"))
+    println(Direction.SOUTH.name)
+    println(Direction.SOUTH.ordinal)
+
+    try {
+        println(enumValueOf<Direction>("WEST"))
+    } catch (e: Throwable) {
+        println("invalid-enum-name")
+    }
+}
