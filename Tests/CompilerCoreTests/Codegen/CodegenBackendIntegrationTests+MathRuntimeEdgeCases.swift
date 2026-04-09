c91e083a00724b604faa30662b73f1aa405eec7f
fumiya-kume <fumiya.kume@hotmail.com>
2026-04-10 03:50:18 +0900

    Fix math runtime review feedback

--- Tests/CompilerCoreTests/Codegen/CodegenBackendIntegrationTests+MathRuntimeEdgeCases.swift
+++ Tests/CompilerCoreTests/Codegen/CodegenBackendIntegrationTests+MathRuntimeEdgeCases.swift
@@ -15,8 +15,8 @@ extension CodegenBackendIntegrationTests {
             println(sqrt(Double.POSITIVE_INFINITY).isInfinite())
             println(sqrt(Double.NaN).isNaN())
 
-            println(log(Double.POSITIVE_INFINITY).isInfinite())
-            println(log(Double.NaN).isNaN())
+            println(ln(Double.POSITIVE_INFINITY).isInfinite())
+            println(ln(Double.NaN).isNaN())
 
             println((-1.0).pow(3.0))
             println((-1.0).pow(2.0))
