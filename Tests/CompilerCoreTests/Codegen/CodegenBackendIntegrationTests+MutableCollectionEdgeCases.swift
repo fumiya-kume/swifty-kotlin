8b8dab9e849f7e49c7143855ffc8df6e08350e50
fumiya-kume <fumiya.kume@hotmail.com>
2026-04-10 03:16:09 +0900

    Fix mutable collection edge coverage CI

--- Tests/CompilerCoreTests/Codegen/CodegenBackendIntegrationTests+MutableCollectionEdgeCases.swift
+++ Tests/CompilerCoreTests/Codegen/CodegenBackendIntegrationTests+MutableCollectionEdgeCases.swift
@@ -11,8 +11,8 @@ extension CodegenBackendIntegrationTests {
             println(zipped.unzip().first)
             println(zipped.unzip().second)
 
-            val map = linkedMapOf("a" to 1)
-            map.putAll(linkedMapOf("b" to 2, "c" to 3))
+            val map = mutableMapOf("a" to 1)
+            map.putAll(mutableMapOf("b" to 2, "c" to 3))
             println(map.keys.toList())
             println(map.values.toList())
 
