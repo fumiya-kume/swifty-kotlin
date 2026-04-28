d4167d4198c3c846dc4e817d72b2c49b30923af9
fumiya-kume <fumiya.kume@hotmail.com>
2026-04-29 03:26:12 +0900

    Add WeakReference runtime ABI

--- Sources/RuntimeABI/RuntimeABISpec.swift
+++ Sources/RuntimeABI/RuntimeABISpec.swift
@@ -8155,6 +8155,7 @@ public enum RuntimeABISpec {
             + durationFunctions
             + timeAndPathBridgeFunctions
             + atomicFunctions
+            + nativeRefFunctions
             + threadLocalFunctions
             + threadFunctions
             + securityFunctions
