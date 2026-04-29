345b4631ad5c9798746b49d8e45a526b6516dfb9
fumiya-kume <fumiya.kume@hotmail.com>
2026-04-30 08:33:20 +0900

    Add CharSequence chunkedSequence

--- Sources/CompilerCore/KIR/CallLowerer+MemberCalls.swift
+++ Sources/CompilerCore/KIR/CallLowerer+MemberCalls.swift
@@ -2853,8 +2853,11 @@ extension CallLowerer {
                 }
                 return classType.classSymbol == charSequenceSymbol
             }()
+            let isCharSequenceTextHelper = calleeStr == "ifBlank"
+                || calleeStr == "ifEmpty"
+                || calleeStr == "chunkedSequence"
             if sema.types.isSubtype(nonNullReceiverType, sema.types.stringType)
-                || ((calleeStr == "ifBlank" || calleeStr == "ifEmpty") && isCharSequenceReceiver)
+                || (isCharSequenceTextHelper && isCharSequenceReceiver)
             {
                 if calleeStr == "toInt" {
                     instructions.append(.call(
@@ -2980,6 +2983,8 @@ extension CallLowerer {
                     ("kk_string_dropLast", [loweredReceiverID, loweredArgIDs[0]])
                 case "chunked":
                     ("kk_string_chunked", [loweredReceiverID, loweredArgIDs[0]])
+                case "chunkedSequence":
+                    ("kk_string_chunkedSequence", [loweredReceiverID, loweredArgIDs[0]])
                 case "encodeToByteArray", "toByteArray":
                     if loweredArgIDs.count == 1 {
                         ("kk_string_encodeToByteArray_charset", [loweredReceiverID, loweredArgIDs[0]])
