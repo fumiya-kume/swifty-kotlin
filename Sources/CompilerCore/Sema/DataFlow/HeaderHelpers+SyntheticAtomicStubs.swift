b2f2ceac945a07ca9f65030f491224cbf81d4616
kuu(Fumiya Kume) <fumiya.kume@gmail.com>
2026-05-05 01:24:27 +0900

    Add AtomicArray fetchAndUpdateAt support

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticAtomicStubs.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticAtomicStubs.swift
@@ -563,6 +563,25 @@ extension DataFlowSemaPhase {
             interner: interner
         )
 
+        let transformType = types.make(.functionType(FunctionType(
+            params: [typeParamType],
+            returnType: typeParamType,
+            isSuspend: false,
+            nullability: .nonNull
+        )))
+        registerAtomicMember(
+            ownerSymbol: symbol,
+            ownerType: ownerType,
+            name: "fetchAndUpdateAt",
+            externalLinkName: "kk_atomic_ref_array_fetchAndUpdateAt",
+            returnType: typeParamType,
+            parameters: [(name: "index", type: types.intType), (name: "transform", type: transformType)],
+            typeParameterSymbols: [typeParamSymbol],
+            classTypeParameterCount: 1,
+            symbols: symbols,
+            interner: interner
+        )
+
         // get operator alias (index: Int): T
         registerAtomicMember(
             ownerSymbol: symbol,
