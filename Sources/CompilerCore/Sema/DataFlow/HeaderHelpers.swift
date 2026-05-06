95a759f55db9f842bb0559098cb7bdb98e0030e6
kuu(Fumiya Kume) <fumiya.kume@gmail.com>
2026-05-07 00:45:33 +0900

    Add kotlin.js.JsClass external interface stub

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers.swift
@@ -1098,6 +1098,7 @@ extension DataFlowSemaPhase {
         registerSyntheticJsNonModuleStubs(symbols: symbols, interner: interner)
         registerSyntheticCoroutinesABIStubs(symbols: symbols, types: types, interner: interner)
         registerSyntheticJsExternalInheritorsOnlyStubs(symbols: symbols, interner: interner)
+        registerSyntheticJsClassStubs(symbols: symbols, types: types, interner: interner)
         registerSyntheticKPropertyIsInitializedStub(symbols: symbols, types: types, interner: interner)
         registerSyntheticJsStaticStubs(symbols: symbols, interner: interner)
     }
