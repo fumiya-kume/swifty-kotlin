affb0e900a2956e934fc66e979484c137cb025b5
fumiya-kume <fumiya.kume@hotmail.com>
2026-05-12 23:29:15 +0900

    Avoid duplicate mutable capture cell setup

--- Sources/CompilerCore/KIR/ExprLowerer+ControlFlowHelpers.swift
+++ Sources/CompilerCore/KIR/ExprLowerer+ControlFlowHelpers.swift
@@ -22,34 +22,15 @@ extension ExprLowerer {
         else {
             return nil
         }
-
-        let countExpr = arena.appendExpr(.intLiteral(1), type: sema.types.intType)
-        instructions.append(.constValue(result: countExpr, value: .intLiteral(1)))
-
-        let cellExpr = arena.appendExpr(.temporary(Int32(arena.expressions.count)), type: sema.types.anyType)
-        instructions.append(.call(
-            symbol: nil,
-            callee: interner.intern("kk_array_new"),
-            arguments: [countExpr],
-            result: cellExpr,
-            canThrow: false,
-            thrownResult: nil
-        ))
-
-        let zeroExpr = arena.appendExpr(.intLiteral(0), type: sema.types.intType)
-        instructions.append(.constValue(result: zeroExpr, value: .intLiteral(0)))
-        let setResult = arena.appendExpr(.temporary(Int32(arena.expressions.count)), type: sema.types.anyType)
-        instructions.append(.call(
-            symbol: nil,
-            callee: interner.intern("kk_array_set"),
-            arguments: [cellExpr, zeroExpr, currentValue],
-            result: setResult,
-            canThrow: false,
-            thrownResult: nil
-        ))
-
-        driver.ctx.setMutableCaptureCell(cellExpr, for: symbol)
-        return cellExpr
+        return emitMutableCaptureCellInitialization(
+            driver: driver,
+            symbol: symbol,
+            currentValue: currentValue,
+            sema: sema,
+            arena: arena,
+            interner: interner,
+            instructions: &instructions
+        )
     }
 
     func loadMutableCaptureCellValue(
