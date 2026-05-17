11741d0b0d754c8ddbe7d3f4b90eaaebfe93bd00
fumiya-kume <fumiya.kume@hotmail.com>
2026-05-17 17:39:47 +0900

    Implement KProperty1 surface

--- Tests/CompilerCoreTests/Codegen/CodegenBackendIntegrationTests+AtomicExtendedEdgeCases.swift
+++ Tests/CompilerCoreTests/Codegen/CodegenBackendIntegrationTests+AtomicExtendedEdgeCases.swift
@@ -1109,7 +1109,7 @@ extension CodegenBackendIntegrationTests {
         }
     }
 
-    func testCodegenAtomicLongArrayFetchAndUpdateAt() throws {
+    func testCodegenAtomicLongArrayFetchAndUpdateAt2() throws {
         let source = """
         @file:OptIn(kotlin.concurrent.atomics.ExperimentalAtomicApi::class)
         import kotlin.concurrent.atomics.AtomicLongArray
