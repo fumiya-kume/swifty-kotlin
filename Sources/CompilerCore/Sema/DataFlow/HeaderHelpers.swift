3ecfda3c4b93f8eb2a5e985f720dd84ef0e3f55b
fumiya-kume <fumiya.kume@hotmail.com>
2026-05-18 15:45:52 +0900

    Add Long toJsBigInt stub

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers.swift
@@ -1122,6 +1122,7 @@ extension DataFlowSemaPhase {
         registerSyntheticNativeConcurrentStubs(symbols: symbols, types: types, interner: interner)
         registerSyntheticJsDateStubs(symbols: symbols, types: types, interner: interner)
         registerSyntheticJsCollectionsReadonlyMapToMapStubs(symbols: symbols, types: types, interner: interner)
+        registerSyntheticJsBigIntInteropStubs(symbols: symbols, types: types, interner: interner)
         registerSyntheticNativeGetterStubs(symbols: symbols, interner: interner)
         registerSyntheticJsNameStubs(symbols: symbols, types: types, interner: interner)
         registerSyntheticExperimentalMarkerStubs(symbols: symbols, types: types, interner: interner)
