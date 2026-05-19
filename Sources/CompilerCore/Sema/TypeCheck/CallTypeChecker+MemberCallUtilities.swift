25c6878835d14664143ae27a1f2b45ac96092c42
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2026-05-19 16:57:21 +0900

    Verify Sequence.windowed stdlib surface (#2489)

--- Sources/CompilerCore/Sema/TypeCheck/CallTypeChecker+MemberCallUtilities.swift
+++ Sources/CompilerCore/Sema/TypeCheck/CallTypeChecker+MemberCallUtilities.swift
@@ -29,8 +29,6 @@ extension CallTypeChecker {
                  "kk_sequence_firstNotNullOf", "kk_sequence_firstNotNullOfOrNull",
                  "kk_sequence_chunked_transform",
                  "kk_sequence_windowed_transform",
-                 "kk_sequence_averageOf",
-                 "kk_sequence_windowed_transform",
                  "kk_sequence_plus_element", "kk_sequence_minus",
                  "kk_array_binarySearch_compare", "kk_array_binarySearch",
                  "kk_list_windowed_transform",
