9bc2fd4496dd374114da48d7c414c2be88722a63
Kume Fumiya <fumiya.kume@hotmail.com>
2026-05-24 14:55:30 +0900

    Implement Path.pathString surface

--- Tests/RuntimeTests/RuntimePathTests.swift
+++ Tests/RuntimeTests/RuntimePathTests.swift
@@ -36,6 +36,17 @@ final class RuntimePathTests: XCTestCase {
         )
     }
 
+    func testPathStringReturnsRawPathString() {
+        XCTAssertEqual(
+            extractStringRaw(kk_path_pathString(makePathRaw(#"C:\tmp\archive.tar.gz"#))),
+            #"C:\tmp\archive.tar.gz"#
+        )
+        XCTAssertEqual(
+            extractStringRaw(kk_path_pathString(makePathRaw("/tmp/archive.tar.gz"))),
+            "/tmp/archive.tar.gz"
+        )
+    }
+
     func testPathNameReturnsLastComponent() {
         XCTAssertEqual(
             extractStringRaw(kk_path_name(makePathRaw("/tmp/archive.tar.gz"))),
