ff9642107eeb80620e76cf53bb4972df180bf86f
fumiya-kume <fumiya.kume@hotmail.com>
2026-05-26 05:58:49 +0900

    Deduplicate JS readonly set stubs

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticJsCollectionsReadonlySetToSetStubs.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticJsCollectionsReadonlySetToSetStubs.swift
@@ -17,7 +17,7 @@ extension DataFlowSemaPhase {
             symbols: symbols,
             interner: interner
         )
-        let readonlySet = ensureJsReadonlySetForToSet(
+        let readonlySet = ensureJsReadonlySetCollectionsType(
             packageFQName: pkg,
             symbols: symbols,
             types: types,
@@ -52,59 +52,6 @@ extension DataFlowSemaPhase {
         )
     }
 
-    private func ensureJsReadonlySetForToSet(
-        packageFQName: [InternedString],
-        symbols: SymbolTable,
-        types: TypeSystem,
-        interner: StringInterner
-    ) -> (symbol: SymbolID, typeParameterSymbol: SymbolID) {
-        let interfaceName = interner.intern("JsReadonlySet")
-        let interfaceFQName = packageFQName + [interfaceName]
-        let interfaceSymbol = ensureInterfaceSymbol(
-            named: "JsReadonlySet",
-            in: packageFQName,
-            symbols: symbols,
-            interner: interner
-        )
-        if let packageSymbol = symbols.lookup(fqName: packageFQName) {
-            symbols.setParentSymbol(packageSymbol, for: interfaceSymbol)
-        }
-        appendJsCollectionsReadonlySetToSetAnnotation(to: interfaceSymbol, symbols: symbols)
-
-        let typeParamName = interner.intern("E")
-        let typeParamFQName = interfaceFQName + [typeParamName]
-        let typeParamSymbol: SymbolID
-        if let existing = symbols.lookup(fqName: typeParamFQName),
-           symbols.symbol(existing)?.kind == .typeParameter {
-            typeParamSymbol = existing
-        } else {
-            typeParamSymbol = symbols.define(
-                kind: .typeParameter,
-                name: typeParamName,
-                fqName: typeParamFQName,
-                declSite: nil,
-                visibility: .private,
-                flags: [.synthetic]
-            )
-        }
-        symbols.setParentSymbol(interfaceSymbol, for: typeParamSymbol)
-
-        let typeParamType = types.make(.typeParam(TypeParamType(
-            symbol: typeParamSymbol,
-            nullability: .nonNull
-        )))
-        let interfaceType = types.make(.classType(ClassType(
-            classSymbol: interfaceSymbol,
-            args: [.out(typeParamType)],
-            nullability: .nonNull
-        )))
-        types.setNominalTypeParameterSymbols([typeParamSymbol], for: interfaceSymbol)
-        types.setNominalTypeParameterVariances([.out], for: interfaceSymbol)
-        symbols.setPropertyType(interfaceType, for: interfaceSymbol)
-
-        return (interfaceSymbol, typeParamSymbol)
-    }
-
     private func registerJsReadonlySetToSetMember(
         ownerSymbol: SymbolID,
         ownerType: TypeID,
@@ -131,7 +78,7 @@ extension DataFlowSemaPhase {
                 && signature.classTypeParameterCount == 1
         }) {
             symbols.setExternalLinkName(externalLinkName, for: existing)
-            appendJsCollectionsReadonlySetToSetAnnotation(to: existing, symbols: symbols)
+            appendJsCollectionsSetAnnotation(to: existing, symbols: symbols)
             return
         }
 
@@ -145,7 +92,7 @@ extension DataFlowSemaPhase {
         )
         symbols.setParentSymbol(ownerSymbol, for: functionSymbol)
         symbols.setExternalLinkName(externalLinkName, for: functionSymbol)
-        appendJsCollectionsReadonlySetToSetAnnotation(to: functionSymbol, symbols: symbols)
+        appendJsCollectionsSetAnnotation(to: functionSymbol, symbols: symbols)
         symbols.setFunctionSignature(
             FunctionSignature(
                 receiverType: ownerType,
@@ -158,17 +105,4 @@ extension DataFlowSemaPhase {
         )
     }
 
-    private func appendJsCollectionsReadonlySetToSetAnnotation(
-        to symbol: SymbolID,
-        symbols: SymbolTable
-    ) {
-        let experimentalRecord = MetadataAnnotationRecord(
-            annotationFQName: "kotlin.js.ExperimentalJsCollectionsApi"
-        )
-        var annotations = symbols.annotations(for: symbol)
-        if !annotations.contains(experimentalRecord) {
-            annotations.append(experimentalRecord)
-            symbols.setAnnotations(annotations, for: symbol)
-        }
-    }
 }
