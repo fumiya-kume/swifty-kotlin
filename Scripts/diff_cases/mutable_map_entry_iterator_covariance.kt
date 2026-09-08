30fe24b77a8a6ef47c2981cee363ecf7784faf65
Kume Fumiya <fumiya.kume@hotmail.com>
2026-09-08 09:01:51 +0900

    Keep the iterator regression focused on entry type resolution

--- Scripts/diff_cases/mutable_map_entry_iterator_covariance.kt
+++ Scripts/diff_cases/mutable_map_entry_iterator_covariance.kt
@@ -1,12 +1,11 @@
 // MutableSet inherits iterator declarations through both Set and MutableIterable.
 fun main() {
     val map = mutableMapOf("one" to 1, "two" to 2)
-    val entries: MutableSet<MutableMap.MutableEntry<String, Int>> = map.entries
+    val entries = map.entries
     val iterator = entries.iterator()
     val entry = iterator.next()
     println(entry.key)
-    println(entry.setValue(42))
-    println(map["one"])
-    iterator.remove()
-    println(map)
+    println(entry.value)
+    println(iterator.next().key)
+    println(iterator.hasNext())
 }
